bbe4a75f732a696d6ec29efab1d49a833085c6de
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-28 21:22:33 +0530

    28-09-2025 Sun

--- pr/dll.c
+++ pr/dll.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct st
 {
@@ -7,27 +8,31 @@ typedef struct st
 	struct st *next;
 }node;
 
+void add_at_end(node**);
 int count_node(node*);
 void rev_print_upto_middle(node*);
+
 
 int main()
 {
 	node *head=0;
 	int c,op;
 	
-	while(true)
+	while(1)
 	{
-		printf("\t1.add_at_end\n");
+		printf("\t1.add_at_end \n\t2.count_node \n\t3.rev_print_upto_middle \n\t4.exit\n");
 		printf("Enter option : ");
-		scanf("%%d",&op);
+		scanf("%d",&op);
 		switch(op)
 		{
-			case 1 : c = count_node(head);
-				 printf("Total node : %d",c);
+			case 1 : add_at_end(&head);
 				 break;
-			case 2 : rev_print_upto_middle(head);
+			case 2 : c = count_node(head);
+				 printf("Total node : %d\n",c);
 				 break;
-			case 3 : exit(0);
+			case 3 : rev_print_upto_middle(head);
+				 break;
+			case 4 : exit(0);
 			default : printf("wrong option\n");
 		}
 	}
@@ -40,7 +45,7 @@ void add_at_end(node **head)
 	node *new = malloc(sizeof(node));
 
 	printf("Enter int data : ");
-	scanf("%d",&new->next);
+	scanf("%d",&new->data);
 	
 	new->prev = new->next = 0;
 	
@@ -48,33 +53,42 @@ void add_at_end(node **head)
 		*head = new;
 	else
 	{
-		node *temp=*head;
-		while(temp->next)
+		node *temp = *head;
+		
+		while(temp->next) 
 			temp = temp->next;
 		
+		new->prev = temp;
+		temp->next = new;
 	}
 }
 
-void rev_print_upto_middle(next *head)
+void rev_print_upto_middle(node *head)
 {
-	node *hnext;
+	if(head==0)
+	{
+		printf("List is empty\n");
+		return ;
+	}
+	node *hnext=0;
 	int i = 0;
 	
 	int n = count_node(head)/2;
 	
-	while(head && i<n-1)
+	while(head && i<n)
 	{
 		i++;
 		head = head->next;
 	}
 	
-	hnext = head->next;
+	hnext = head;
 	
 	while(hnext)
 	{
 		printf("%d ",hnext->data);
 		hnext = hnext->prev;
 	}
+	printf("\n");
 }
 
 int count_node(node *head)
