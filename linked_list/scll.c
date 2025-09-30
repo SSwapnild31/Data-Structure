42736e42917a65ecf16f1b2af63cd52d4dc292d4
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-30 20:13:39 +0530

    30-09-2025 Tue

--- linked_list/scll.c
+++ linked_list/scll.c
@@ -12,6 +12,7 @@ void add_end(scll**);
 void display(scll*);
 int count_node(scll*);
 void add_middle(scll**);
+void print_rev(scll*);
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
 	scll *head = 0;
 	while(1)
 	{
-		printf("\t1.add_begin \n\t2.add_end \n\t3.add_middle \n\t4.display \n\t5.count_node \n\t10.exit\n");
+		printf("\t1.add_begin \n\t2.add_end \n\t3.add_middle \n\t4.display \n\t5.count_node \n\t6.print_rev \n\t10.exit\n");
 		printf("Enter option : ");
 		scanf("%d",&ch);
 
@@ -30,13 +31,39 @@ int main()
 			case 3  : add_middle(&head);	break;
 			case 4  : display(head);	break;
 			case 5  : c = count_node(head);	
-				  c > 0 ? printf("Node count : %d\n",c) : 0 ; break;	
+				  c > 0 ? printf("Node count : %d\n",c) : 0 ; 
 				  break;
+			case 6  : print_rev(head); printf("\n"); break;
 			case 10 : exit(0);
 			default : printf("wrong option.!\n");
 		}
 	}
 	return 0;
+}
+
+void print_rev(scll *head)
+{
+	if(head==0)
+	{
+		printf("list is empty.!\n");
+		return ;
+	}
+
+	static int i=0;
+	static scll *ptr;
+
+	if(i==0) 
+	{	
+		ptr = head;
+		i=1;
+	}
+
+	if(head->next!=ptr)
+		print_rev(head->next);
+
+	printf("%d ",head->data);
+	
+	return ;
 }
 
 void add_begin(scll **ptr)
@@ -45,7 +72,6 @@ void add_begin(scll **ptr)
 	
 	printf("Enter int data : ");
 	scanf("%d",&new->data);
-	new->next = 0;
 	
 	if(*ptr==0)	
 	{ 
@@ -103,29 +129,34 @@ int count_node(scll *head)
 
 void add_middle(scll **ptr)
 {
-	scll *new = malloc(sizeof(scll));
+	scll *temp = *ptr, *new = malloc(sizeof(scll));
 	
 	printf("Enter data : ");
 	scanf("%d",&new->data);
-	new->data = 0;
 	
-	if(*ptr==0 || new->data < (*ptr)->data)
+	if(*ptr==0)
 	{
 		*ptr = new;
 		new->next = *ptr;
+		return ;
 	}
-	else
+	
+	if(new->data < (*ptr)->data)
 	{
-		scll *temp = *ptr;
-		while(temp->next!=*ptr && new->data > temp->next->data)
+		new->next = *ptr;
+		while(temp->next!=*ptr)
 			temp = temp->next;
-
-		if(temp->next==*ptr)
-		{
-			new->next = temp->next;
-			temp->next = new;
-		}
+		*ptr = new;
+		temp->next = *ptr;
+		return ;
 	}
+
+	temp = *ptr;
+	while(temp->next!=*ptr && new->data > temp->next->data)
+		temp = temp->next;
+
+	new->next = temp->next;
+	temp->next = new;
 }
 
 void add_end(scll **ptr)
@@ -134,7 +165,6 @@ void add_end(scll **ptr)
 	
 	printf("Enter int data : ");
 	scanf("%d",&new->data);
-	new->next = 0;
 	
 	if(*ptr==0)
 	{
