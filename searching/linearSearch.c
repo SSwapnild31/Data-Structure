bbc4af84e999bd42b63daaa57b0ffe355656334a
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-24 22:59:04 +0530

    Updated linearSearch.c

--- searching/linearSearch.c
+++ searching/linearSearch.c
@@ -12,3 +12,15 @@ scanf("%d",&a[i]);
 int key;
 printf("Enter element to search : ");
 scanf("%d",&key);
+
+for(int i=0;i<ele;i++){
+if(a[i]==key){
+printf("Element is present\n");
+return 0;
+}
+}
+
+printf("Element not present\n");
+
+return 0;
+}
