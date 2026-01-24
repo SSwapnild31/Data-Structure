da8f42cd696e371cba9ccab1fdf96d1c11912da3
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-24 22:50:26 +0530

    Create linearSearch.c

---
+++ searching/linearSearch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+int a[5];
+int ele = sizeof(a)/sizeof(a[0]);
+
+for(int i=0;i<ele;i++){
+scanf("%d",&a[i]);
+}
+
+int key;
+printf("Enter element to search : ");
+scanf("%d",&key);
