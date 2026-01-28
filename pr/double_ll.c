f09c4f9931a65c322af15e869c402d57c14b3984
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-28 09:18:01 +0530

    28-01-2026 wed

---
+++ pr/double_ll.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *next;
+	struct node *prev;
+}dll;
+
+void addBegin(dll **ptr, int num){
+	dll *newNode = malloc(sizeof(dll));
+	
+	newNode->data = num;
+	newNode->prev = NULL;
+	newNode->next = *ptr;
+	
+	if(*ptr != NULL){
+		(*ptr)->prev = newNode;
+	}
+	
+	*ptr = newNode;
+}
+
+
+void display(dll *ptr){
+	if(ptr == NULL){
+		printf("Empty list!\n");
+		return;
+	}
+	while(ptr){
+		printf("-> %d ",ptr->data);
+		ptr = ptr->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	dll *head = 0;
+	while(1){
+		int op, num, c;
+		printf("1.addBegin\t2.addEnd\n3.display\t4.exit\n");
+		printf("Enter option : ");
+		scanf("%d",&op);
+		switch(op){
+			case 1 : printf("Enter number : "); scanf("%d",&num); addBegin(&head, num);	break;
+			case 2 : printf("Enter number : "); scanf("%d",&num); addEnd(&head, num);	break;
+			case 3 : display(head);		break;
+			case 4 : exit(0);
+			default: printf("wrong option! Try again!\n");
+		}
+	}
+
+	return 0;
+}
