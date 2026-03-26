bfda89fe47285d7d84133dc2aba8fca38fb12731
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-03-27 03:02:26 +0530

    27-03-2026 Fri

--- pr/ll.c
+++ pr/ll.c
@@ -10,32 +10,63 @@ typedef struct node{
 void addBegin(sll**);
 void addEnd(sll**);
 void display(sll*);
-int nodeCount(sll*);
+int  nodeCount(sll*);
 void reversePrint(sll*);
 void reverseList(sll**);
+void swapNearby(sll**);
 
 int main()
 {
 	sll *head = 0;
 	int c, op;
 	while(1){
-		printf("1.addBegin\t2.addEnd\n3.display\t4.nodeCount\n5.revPrint\t6.revList\n7.exit\n");
+		printf("1.addBegin\t2.addEnd\n3.display\t4.nodeCount\n5.revPrint\t6.revList\n7.swapNearby\t8.exit\n");
 		printf("Enter option : ");
 		scanf("%d",&op);
 
 		switch(op){
-			case 1 : addBegin(&head);	break;
-			case 2 : addEnd(&head);		break;
-			case 3 : display(head);		break;
+			case 1 : addBegin(&head);					break;
+			case 2 : addEnd(&head);						break;
+			case 3 : display(head);						break;
 			case 4 : c = nodeCount(head);	
-				 printf("Total nodes : %d\n",c);break;
+				 	 printf("Total nodes : %d\n",c);	break;
 			case 5 : reversePrint(head);	
-				 printf("\n");		break;
-			case 6 : reverseList(&head);	break;
-			case 7 : exit(0);
+				 	 printf("\n");						break;
+			case 6 : reverseList(&head);				break;
+			case 7 : swapNearby(&head);					break;
+			case 8 : exit(0);
 			default: printf("oops! wrong option!\n");
 		}
 	}
+}
+
+void swapNearby(sll** head){
+	
+    if(!(*head) || !(*head)->next) return;
+
+    sll* newHead = (*head) -> next;
+
+    sll* prev = NULL;
+    sll* curr = *head;
+
+    while(curr && curr->next){
+        sll* first = curr;
+        sll* second = curr -> next;
+
+        //swap
+        first -> next = second -> next;
+        second -> next = first;
+
+        if(prev)
+            prev -> next = second;
+
+        //move ptr's
+        prev = first;
+        curr = first -> next;
+    }
+    
+	*head = newHead;
+	printf("Nearby swapped.\n");
 }
 
 void reverseList(sll** head){
